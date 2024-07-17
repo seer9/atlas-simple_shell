30a9e470fcc7c56d2fd0fb09cd6d0d382519e1ff
internashionalist <taylor.thames@atlasschool.com>
2024-07-17 18:00:09 -0500

    shell.c init commit

---
+++ shell.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/* display a prompt */
+
+/* read command */
+
+/* tokenize command */
+
+/* execute command */
+
+/* handle errors */
+
+/* free memory */
+
+/* terminate shell */
