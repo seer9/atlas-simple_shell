161e7bc9f54c5ebf354381e52a7acc50f18c6da0
internashionalist <taylor.thames@atlasschool.com>
2024-07-18 18:56:22 -0500

    tokenize string helper function

---
+++ tokenize_input.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/* tokenize_input: tokenize input string */
+ * @input: input string
+ * @tokens: array of tokens
+ * @delims: delimiters
+ * 
+ * Return: number of tokens created
+ */
+
+void tokenize_input(char *input, char **tokens, char *delims)
+{
+	 char *token; /* pointer to token */
+	 int token_counter = 0; /* counter for tokens in input string */
+
+	 token = strtok(input, delims); /* begin tokenizing string */
+	 while (token != NULL) /* loop through input string */
+	 {
+		  tokens[token_counter] = token; /* store token in array */
+		  token = strtok(NULL, delims); /* tokenize next bit of string */
+		  token_counter++; /* increment token counter */
+	 }
+	 tokens[token_counter] = NULL; /* set last token to NULL to terminate array */
+}
