c4e24404952ca0d1877dd9ada6c50d427824b599
“internashionalist” <taylor.thames@atlaschool.com>
2024-07-20 08:21:22 -0500

    cautious optimism with our boy T.S. SHELLiot

--- shell.c
+++ shell.c
@@ -1,36 +1,36 @@
 #include "shell.h"
 
-int main(void)
+/**
+ * TSSHELLIOT - 
+ * 
+ * Return: void
+ */
+
+void TSSHELLIOT(void)
 {
-       char *command = NULL;
-       size_t command_size = 0;
-       ssize_t num_read;
+	char *input = NULL; /* pointer to input string */
+	size_t input_size = 0; /* size of input string */
+	char **tokens = NULL; /* array of split input tokens */
+	int token_count; /* number of tokens in input */
 
-       while (1)
-       {
-              display_prompt(); /* display a prompt */
-
-              num_read = getline(&command, &command_size, stdin); /* read command */
-
-              
-
-
-              /* tokenize command */
-
-              /* execute command */
-
-              /* free memory */
-       }
-/* display a prompt */
-
-/* read command */
-
-/* tokenize command */
-
-/* execute command */
-
-/* handle errors */
-
-/* free memory */
-
-/* terminate shell */
+	while (1) /* infinite looooooop */
+	{
+		tokens = malloc(64 * sizeof(char *)); /* malloc for input array */
+		if (tokens == NULL) /* if malloc fails */
+		{
+			free_mem(input, tokens); /* set it free */
+			exit(EXIT_FAILURE); /* burn it down */
+		}
+		display_prompt(); /* call display_prompt */
+		read_input(&input, &input_size); /* call read_input */
+		if (read_input(&input, &input_size) == -1) /* if read fails */
+		{
+			free_mem(input, tokens); /* set it free */
+			exit(EXIT_FAILURE); /* burn it down */
+		}
+		token_count = tokenize_input(input, tokens); /* call tokenize_input */
+		if (token_count == -1) /* if tokenization fails */
+		{
+			free_mem(input, tokens); /* set it free */
+			exit(EXIT_FAILURE); /* burn it down */
+		}
