55cbdd8a12c68b3f8be9addafc3a8cb56784710e
internashionalist <taylor.thames@atlasschool.com>
2024-07-19 22:21:42 -0500

    added malloc, formatting, return

--- tokenize_input.c
+++ tokenize_input.c
@@ -1,25 +1,30 @@
 #include "shell.h"
 
 /**
- * tokenize_input: tokenize input string
+ * tokenize_input: splits input string into tokens
  * @input: input string
- * @tokens: array of tokens
- * @delims: delimiters
+ * @tokens: array to store split tokens
  * 
  * Return: number of tokens created
  */
 
-void tokenize_input(char *input, char **tokens)
+int tokenize_input(char *input, char **tokens)
 {
-	 char *token; /* pointer to token */
-	 int token_counter = 0; /* counter for tokens in input string */
+	char *token; /* pointer to token */
+	int token_counter = 0; /* counter for tokens in input string */
 
-	 token = strtok(input, DELIM); /* begin tokenizing string */
-	 while (token != NULL) /* loop through input string */
-	 {
-		  tokens[token_counter] = token; /* store token in array */
-		  token = strtok(NULL, DELIM); /* tokenize next bit of string */
-		  token_counter++; /* increment token counter */
-	 }
-	 tokens[token_counter] = NULL; /* set last token to NULL to terminate array */
+	*tokens = malloc(64 * sizeof(char *)); /* malloc for input tokens array */
+	if (*tokens == NULL) /* if malloc fails */
+	{
+		exit(EXIT_FAILURE); /* burn it down */
+	}
+	token = strtok(input, DELIM); /* start splittin' that input (DELIM set in shell.h) */
+	while (token != NULL) /* loop through input string */
+	{
+		(*tokens)[token_counter] = strdup(token); /* store token in array */
+		token_counter++; /* increment counter */
+		token = strtok(NULL, DELIM); /* move to next token in string */
+	}
+	(*tokens)[token_counter] = NULL; /* end array */
+	return (token_counter); /* return number of tokens */
 }
