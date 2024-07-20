d0d20dd5747f8aa2e9f5826258614953ae44d161
“internashionalist” <taylor.thames@atlaschool.com>
2024-07-19 22:24:26 -0500

    updated prototypes

--- shell.h
+++ shell.h
@@ -13,12 +13,13 @@
 #include <stdarg.h>
 
 #define BUFFER_SIZE 1024
+#define TOK_SIZE 128
 #define DELIM " \t\n" /* delimiters for tokenizing input */
 
 /* Prototypes */
 void init_shell(char **env_list, char **path, char **path_array);
 void display_prompt(void);
 int read_input(char **input, size_t *input_size);
-void tokenize_input(char *input, char **tokens, char *delims);
+int tokenize_input(char *input, char **tokens);
 
 #endif
