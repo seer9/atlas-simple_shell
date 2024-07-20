c334613ab50f9d2e1d7acca4ccc94d5305d756fa
internashionalist <taylor.thames@atlasschool.com>
2024-07-20 08:48:59 -0500

    corrected free issue, pointer issue, added special iterator for freeing already malloc'd tokens in case of partial success

--- tokenize_input.c
+++ tokenize_input.c
@@ -12,19 +12,24 @@ int tokenize_input(char *input, char **tokens)
 {
 	char *token; /* pointer to token */
 	int token_counter = 0; /* counter for tokens in input string */
+	int i; /* counter for freeing tokens */
 
-	*tokens = malloc(64 * sizeof(char *)); /* malloc for input tokens array */
-	if (*tokens == NULL) /* if malloc fails */
-	{
-		exit(EXIT_FAILURE); /* burn it down */
-	}
 	token = strtok(input, DELIM); /* start splittin' that input (DELIM set in shell.h) */
 	while (token != NULL) /* loop through input string */
 	{
-		(*tokens)[token_counter] = strdup(token); /* store token in array */
+
+		(tokens)[token_counter] = strdup(token); /* store token in array */
+		if ((tokens)[token_counter] == NULL) /* if strdup fails */
+		{
+			for (i = 0; i < token_counter; i++) /* loop through already malloc'd tokens */
+			{
+				free(tokens[i]); /* free each token */
+			}
+			return (-1); /* burn SOME of it down */
+		}
 		token_counter++; /* increment counter */
 		token = strtok(NULL, DELIM); /* move to next token in string */
 	}
-	(*tokens)[token_counter] = NULL; /* end array */
+	(tokens)[token_counter] = NULL; /* end array */
 	return (token_counter); /* return number of tokens */
 }
