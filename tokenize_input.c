c6393371aed45308f18b61d97f6bfcc1acd2139a
internashionalist <taylor.thames@atlasschool.com>
2024-07-20 19:33:28 -0500

    betty-fied and polished code

--- tokenize_input.c
+++ tokenize_input.c
@@ -1,35 +1,37 @@
 #include "shell.h"
 
 /**
- * tokenize_input: splits input string into tokens
+ * tokenize_input - splits input string into tokens
  * @input: input string
- * @tokens: array to store split tokens
+ * @tokens: array of split input tokens
  * 
- * Return: number of tokens created
+ * Return: number of tokens created or -1 on error
  */
-
-int tokenize_input(char *input, char **tokens)
+int tokenize_input(char *input, char ***tokens)
 {
 	char *token; /* pointer to token */
 	int token_counter = 0; /* counter for tokens in input string */
 	int i; /* counter for freeing tokens */
 
-	token = strtok(input, DELIM); /* start splittin' that input (DELIM set in shell.h) */
+	*tokens = malloc(64 * sizeof(char *)); /* malloc for input array */
+	if (*tokens == NULL) /* if malloc fails */
+		return (-1); /* indicate failure */
+
+	token = strtok(input, DELIM); /* start splitting input (DELIM set in shell.h) */
 	while (token != NULL) /* loop through input string */
 	{
-
-		(tokens)[token_counter] = strdup(token); /* store token in array */
-		if ((tokens)[token_counter] == NULL) /* if strdup fails */
+		(*tokens)[token_counter] = strdup(token); /* store token in array */
+		if ((*tokens)[token_counter] == NULL) /* if strdup fails */
 		{
-			for (i = 0; i < token_counter; i++) /* loop through already malloc'd tokens */
-			{
-				free(tokens[i]); /* free each token */
-			}
-			return (-1); /* burn SOME of it down */
+			for (i = 0; i < token_counter; i++) /* loop already malloc'd tokens */
+				free((*tokens)[i]); /* free each token */
+
+			free(*tokens); /* free the array itself */
+			return (-1); /* indicate failure */
 		}
 		token_counter++; /* increment counter */
 		token = strtok(NULL, DELIM); /* move to next token in string */
 	}
-	(tokens)[token_counter] = NULL; /* end array */
+	(*tokens)[token_counter] = NULL; /* null-terminate the array */
 	return (token_counter); /* return number of tokens */
 }
