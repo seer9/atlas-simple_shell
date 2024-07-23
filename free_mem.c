54dd1986bea9a2d125b5ca7cd207c37384f6a1c3
internashionalist <taylor.thames@atlasschool.com>
2024-07-23 14:27:52 -0500

    maybe double free fix

--- free_mem.c
+++ free_mem.c
@@ -8,23 +8,23 @@
  * Return: void
  */
 
-void free_mem(char *input, char **tokens)
+void free_mem(char **input, char ***tokens)
 {
 	int i; /* counter for freeing tokens */
 
-	if (input != NULL) /* if input string exists */
+	if (input != NULL && *input != NULL) /* if input string exists */
 	{
-		free(input); /* free input string */
-		input = NULL; /* dangling pointer */
+		free(*input);	 /* free input string */
+		*input = NULL; /* clear the dangling pointer */
 	}
-	if (tokens != NULL) /* if token array exists */
+	if (tokens != NULL && *tokens != NULL) /* if token array exists */
 	{
-		for (i = 0; tokens[i] != NULL; i++) /* loop through array */
+		for (i = 0; (*tokens)[i] != NULL; i++) /* loop through array */
 		{
-			free(tokens[i]); /* free each token */
-			tokens[i] = NULL; /* dangling pointer */
+			free((*tokens)[i]);	/* free each token */
+			(*tokens)[i] = NULL; /* clear the dangling pointer */
 		}
-		free(tokens); /* free token array */
-		tokens = NULL; /* dangling pointer */
+		free(*tokens);  /* free token array */
+		*tokens = NULL; /* clear the dangling pointer */
 	}
 }
