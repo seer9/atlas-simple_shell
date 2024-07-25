91bde688f1d04fbbe3fe1f3579251f95f9787616
internashionalist <taylor.thames@atlasschool.com>
2024-07-24 19:12:30 -0500

    streamline tokenization, update main

--- tokenize_input.c
+++ tokenize_input.c
@@ -11,21 +11,20 @@ int tokenize_input(char *input, char ***tokens)
 {
 	char *token; /* pointer to token */
 	int token_counter = 0; /* counter for tokens in input string */
-	int i; /* counter for freeing tokens */
+	int i; /* index for freeing tokens */
 
-	*tokens = malloc(64 * sizeof(char *)); /* malloc for input array */
+	*tokens = malloc(TOK_SIZE * sizeof(char *)); /* malloc for input array */
 	if (*tokens == NULL) /* if malloc fails */
 		return (-1); /* indicate failure */
-	token = strtok(input, DELIM); /* split input (DELIM set in shell.h) */
+	token = strtok(input, DELIM); /* split input string */
 	while (token != NULL) /* loop through input string */
 	{
-		(*tokens)[token_counter] = strdup(token); /* store token in array */
+		(*tokens)[token_counter] = strdup(token); /* duplicate token */
 		if ((*tokens)[token_counter] == NULL) /* if strdup fails */
 		{
-			for (i = 0; i < token_counter; i++) /* already malloc'd tokens */
+			for (i = 0; i < token_counter; i++) /* loop previously allocated */
 				free((*tokens)[i]); /* free each token */
 			free(*tokens); /* free the array itself */
-			*tokens = NULL; /* NULLify the array */
 			return (-1); /* indicate failure */
 		}
 		token_counter++; /* increment counter */
